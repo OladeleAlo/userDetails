2a13fc13bafaa899bea60a72546d0955956edb59
Oladele <oladele2alo@gmail.com>
2022-08-16 00:05:03 +0200

    first

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,41 @@
+import React,{useState} from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
+
+const AddUser = (props) =>{
+    const [enteredUsername, setEnteredUsername] = useState("");
+    const [enteredAge, setEnteredAge] = useState("");
+
+    const addUserHandler = (event) => {
+        event.preventDefault();
+        
+        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+            return;
+        }
+        if(+enteredAge < 1){
+            return;
+        }
+
+        props.onAddUser(enteredUsername, enteredAge)
+        setEnteredAge(" ");
+        setEnteredUsername(" ");
+    }
+
+    const usernameChangeHandler = (event)=> {
+        setEnteredUsername(event.target.value)
+    }
+
+    const ageChangeHandler = (event)=> {
+        setEnteredAge(event.target.value)
+    }
+    return (
+        <>
+        <ErrorModal title ="An Error Ocurred! " message="Something went wrong"/>
+        <Card className ={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input type="text" id= "username" value={enteredUsername} onChange={usernameChangeHandler}/>
+                <label htmlFor="age">Age(Years)</label>
+                <input type="number" id= "age" value={enteredAge} onChange = {ageChangeHandl
